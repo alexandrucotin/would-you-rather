8b92c5544ee707af58e68f3ab2633c4efa0d659f
Alexandru Cotin <a.cotin96@gmail.com>
2020-06-20 15:19:43 +0200

    added style for new question component

--- src/components/newQuestion/index.js
+++ src/components/newQuestion/index.js
@@ -5,15 +5,19 @@ class NewQuestion extends Component {
   render() {
     return (
       <div className="new-question-container">
-        <h2 className="title">Submit new poll</h2>
-        <p className="subtitle">Complete the question:</p>
-        <p className="wur">Would you rather ...</p>
-        <div className="answer-options-container">
-          <input className="answer-option" />
-          <h4>or</h4>
-          <input className="answer-option" />
+        <div>
+          <div className="titles-container">
+            <h2 className="title">Submit new poll</h2>
+            <p className="subtitle">Complete the question</p>
+          </div>
+          <p className="wur">Would you rather ...</p>
+          <div className="answer-options-container">
+            <input className="answer-option" />
+            <h4>or</h4>
+            <input className="answer-option" />
+          </div>
         </div>
-        <button type="submit">Submit</button>
+        <button className="button" type="submit">Submit</button>
       </div>
     );
   }
