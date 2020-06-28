fe6991dadd698a0caa571cfb3bac306483e2bac0
Alexandru Cotin <a.cotin96@gmail.com>
2020-06-28 19:01:55 +0200

    Added logic

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,17 +1,13 @@
 import { _getQuestions, _getUsers } from "../utils/_DATA";
 import { receiveQuestions } from "../actions/questions";
 import { receiveUsers } from "../actions/users";
-import { setAuthedUser } from "../actions/authedUser";
 import { showLoading, hideLoading } from "react-redux-loading";
-
-const AUTHED_ID = "johndoe";
 
 export function handleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
     return Promise.all([_getQuestions(), _getUsers()]).then(
       ([questions, users]) => {
-        dispatch(setAuthedUser(AUTHED_ID));
         dispatch(receiveUsers(users));
         dispatch(receiveQuestions(questions));
         dispatch(hideLoading());
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
+import {setAuthedUser} from "../../actions/authedUser"
+import {connect} from "react-redux"
 import "./login.css";
 
 class Login extends Component {
+  state = {
+    authedUser: "select a user",
+  };
+
+  handleOnChange = (e) => {
+    const selectedUser = e.target.value;
+    this.setState(() => ({
+      authedUser: selectedUser,
+    }));
+  };
+
+  handleOnClick = (e) => {
+    const {dispatch} = this.props;
+    dispatch(setAuthedUser(this.state.authedUser));
+  };
+
   render() {
     return (
       <div className="login-container">
         <h3 className="title">Login</h3>
-        <select className="login-dropdown">
-          <option className="dropdown-option">Sarah</option>
-          <option className="dropdown-option">Tyler</option>
-          <option className="dropdown-option">John</option>
+        <select onChange={this.handleOnChange}  defaultValue={this.state.value} className="login-dropdown">
+          <option value="select a user" selected={true} disabled="disabled" className="dropdown-option">
+            select a user...
+          </option>
+          <option value="sarahedo" className="dropdown-option">
+            Sarah
+          </option>
+          <option value="tylermcginnis" className="dropdown-option">
+            Tyler
+          </option>
+          <option value="johndoe" className="dropdown-option">
+            John
+          </option>
         </select>
-        <button className="button" type="submit">
+        <button className="button" type="submit" onClick={this.handleOnClick}>
           Login
         </button>
       </div>
@@ -19,4 +46,4 @@ class Login extends Component {
   }
 }
 
-export default Login;
+export default connect()(Login);
